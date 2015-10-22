dea842391a2f34145abc82aae4a06c14e1dd56e0
Rick Peyton <peytorb@gmail.com>
2015-10-22 13:04:36 -0500

    iteration 2 on the clock project

--- clock/clock.go
+++ clock/clock.go
@@ -3,36 +3,28 @@ package clock
 import "fmt"
 
 // Declare version to test submission against
-const TestVersion = 2
+const (
+	TestVersion   = 2
+	hourlyMinutes = 60
+	dailyMinutes  = 1440
+)
 
 // Clock represents the time in minutes on a 24 hour clock
 type Clock int
 
 // Time converts hours and minutes into minutes on a 24 hour clock
 func Time(hours, minutes int) Clock {
-	// Takes in hours and minutes and returns minutes
-	time := hours*60 + minutes
-	for time >= 1440 {
-		time -= 1440
-	}
-	for time < 0 {
-		time += 1440
-	}
+	// Takes in hours and minutes and returns positive minutes
+	// Ensures that time is greater than 0 and less than 1440
+	time := (((hours*hourlyMinutes + minutes) % dailyMinutes) + dailyMinutes) % dailyMinutes
 	return Clock(time)
 }
 
 // Add or subtract minutes from an existing clock to update 24 hour clock
 func (clock Clock) Add(minutes int) Clock {
-	updateClock := clock + Clock(minutes)
-	for updateClock >= 1440 {
-		updateClock -= 1440
-	}
-	for updateClock < 0 {
-		updateClock += 1440
-	}
-	return updateClock
+	return Time(0, int(clock)+minutes)
 }
 
 func (clock Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", clock/60, clock%60)
+	return fmt.Sprintf("%02d:%02d", clock/hourlyMinutes, clock%hourlyMinutes)
 }
